4485934f3b6d63501aec74722ff11e2a084d40bf
WinnieTheBoom <win34086@gmail.com>
2024-01-17 18:49:53 +0700

    form

--- example-app/app/Http/Controllers/MyController.php
+++ example-app/app/Http/Controllers/MyController.php
@@ -1,5 +1,32 @@
 <?php
 
+// namespace App\Http\Controllers;
+
+// use Illuminate\Http\Request;
+
+// class MyController extends Controller
+// {
+//     private $myvar = "Hello World!";
+
+//     function _construct() //MyController
+//     {
+
+//     }
+//     function index(){
+//        // echo $this->myvar;
+//        //return view('welcome');
+//        $data=['val_a' => 'Hello World!'];
+//     $data['val_b'] = "Laravel";
+//     return view('myfolder.mypage',$data);
+//     }
+
+//     public function store(Request $reg){
+//         $data['myinput'] = $req->input('myinput');
+//         return view('myroute',$data);
+//     }
+// }
+
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -8,20 +35,17 @@ class MyController extends Controller
 {
     private $myvar = "Hello World!";
 
-    function _construct() //MyController
+    function __construct() //MyController
     {
 
     }
-    function index(){
-       // echo $this->myvar;
-       //return view('welcome');
-       $data=['val_a' => 'Hello World!'];
-    $data['val_b'] = "Laravel";
-    return view('myfolder.mypage',$data);
+
+    public function index(){
+        return view('home');
     }
 
-    public function store(Request $reg){
+    public function store(Request $req){
         $data['myinput'] = $req->input('myinput');
-        return view('myroute',$data);
+        return view('myroute', $data);
     }
 }
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\MyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\C_titles;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,18 +15,36 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-// Route::get('/my-controller', [MyController::class, 'index']);
-// Route::get('/my-controller2','App\Http\Controllers\MyController@index');
 
-// Route::get('/', function () {
-//     return view('welcome');
+Route::get('/my-controller', [MyController::class, 'index']);
+
+// Route::get('/my-controller2', 'App\Http\Controllers\MyController@index');
+// Route::namespace('App\Http\Controllers')->group(function(){
+//     Route::get('/my-controller3', 'MyController@index');
+//     Route::post('/my-controller3-post', 'MyController@store');
 // });
-Route::get('/my-route',function(){
-    $data=['val_a' => 'Hello World!'];
-    // $data['val_b'] = "Laravel";
+
+// Route::resource('/my-controller4', MyController::class);
+
+
+Route::get('/', function () {
+    return view('welcome'); // welcome.blade.php
+});
+
+// use Illuminate\Http\Request;
+
+Route::get('/my-route', function(){
+    // return view('myroute');
+    //        Key    =>  Value
+    $data = ['val_a' => 'Hello World!'];
+    $data['val_b'] = "Laravel";
     return view('myfolder.mypage',$data);
 });
-Route::post('/my-route',function(Request $req){
-    $data['myinput'] = $req->input('myinput');
-    return view('myroute',$data);
+
+
+Route::post('/my-route', function(Request $req){
+    $data['myinput'] =  $req->input('myinput');
+    return view('myroute', $data);
 });
+
+Route::resource('titles',C_titles::class);
